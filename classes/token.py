e3a53e447b0119c393219d0d479e9352994fa3e9
Illin Tori <IllinTori@gmail.com>
2019-03-15 18:28:14 +0000

    Added user keys

--- classes/token.py
+++ classes/token.py
@@ -10,6 +10,14 @@ charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890+="
 def token_file(user_name):
     return "storage/users/"+user_name+"/tokens/"
 
+def reap_tokens():
+    for user in os.listdir("storage/users/"):
+        for token in os.listdir(token_file(user)):
+            with open(token,"r") as f:
+                age = int(f.read())
+            if age + max_time < time.time():
+                os.remove(token)
+
 def gen_token(user_name):
     # Make Token string
     token = ""
--- pages/login.py
+++ pages/login.py
@@ -5,14 +5,31 @@ GET = False
 # it will take the username and password and return a token
 
 import classes
+import os
+import bcrypt
 
 
 def main(path, args):
     # Loggin in with a password and username
     if "name" in args and "password" in args:
-
-        return 200, {"success":True}
+        user_name = args["name"].lower()
+        
+        # Does user exists
+        if user_name in os.listdir("storage/users/"):
+            # Get password file
+            with open("storage/users/"+user_name+"/password","rb") as f:
+                password_hash = f.read()
+            success = bcrypt.checkpw(args["password"].encode("utf-8"),password_hash)
+            if success:
+                return 200, {"sucess":True,"token":classes.token.gen_token(user_name)}
+            else:
+                return 400, {"success":False,"reason":"Invalid user credentials"}
+        else:
+            return 400, {"success":False,"reason":"Invalid user credentials"}
     
-    # Loggin in with a `rememberme` token
+    # Loggin in with a `remember-me` token
     elif "name" in args and "token" in args:
         return 200, {"success":True}
+
+    else:
+        return 400, {"success":False,"reason":"Invalid syntax, name and password/token requierd"}
